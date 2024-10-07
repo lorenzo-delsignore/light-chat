f46eb03ae4d3429fd79e26a524698487e1db18b6
lorenzo-delsignore <lorenzo.delsignore94@gmail.com>
2024-10-07 22:37:21 +0200

    Fix import names

--- light_chat/eval.py
+++ light_chat/eval.py
@@ -3,7 +3,7 @@ import torch
 from omegaconf import DictConfig
 from torch import nn
 
-from light_chat.models.trigram_module import NgramModuleVanilla
+from light_chat.models.ngram_module import NgramModuleVanilla
 from light_chat.utils import (
     RankedLogger,
     extras,
